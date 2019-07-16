82df960153c8d1282ac6e7ae3ecb2c30da6293c7
Foteini Pipi <pipifot@gmail.com>
2019-07-16 18:42:38 +0300

    Add project files.

---
+++ WheelofAccess/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WheelofAccess.Models
+{
+    public class Category
+    {
+        public bool Cafe { get; set; }
+        public bool Restaurant { get; set; }
+        public bool Bar { get; set; }
+        ICollection<Place> TypesofPlace { get; set; }
+    }
+}
