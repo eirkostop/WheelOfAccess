26693cad56adb4f18471ff3bb4741fa50fe0b24c
Eirini Kostopoulou <lysistrat@hotmail.com>
2019-07-29 12:50:15 +0300

    deleted migrations

--- WheelofAccess/Migrations/201907290946342_init.Designer.cs
+++ WheelofAccess/Migrations/201907290946342_init.Designer.cs
@@ -7,13 +7,13 @@ namespace WheelofAccess.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class first : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(first));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201907290911171_first"; }
+            get { return "201907290946342_init"; }
         }
         
         string IMigrationMetadata.Source
--- WheelofAccess/Migrations/201907290946342_init.cs
+++ WheelofAccess/Migrations/201907290946342_init.cs
@@ -3,16 +3,14 @@ namespace WheelofAccess.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class FirstattempttoUserRoles : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.AspNetUsers", "UserRole", c => c.String());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.AspNetUsers", "UserRole");
         }
     }
 }
