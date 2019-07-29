6194e19c3fc100825d7517fcb3a21c7eae4a1697
Foteini Pipi <pipifot@gmail.com>
2019-07-29 12:11:16 +0300

    All working

--- WheelofAccess/Services/Index.aspx.cs
+++ WheelofAccess/Services/Index.aspx.cs
@@ -13,12 +13,12 @@ namespace SamplePayment.amazon
         {
             //https://fps.sandbox.amazonaws.com/
             //https://authorize.payments-sandbox.amazon.com/cobranded-ui/actions/start
-            //secret key  =RTHB4vGiGHOo+SxxVnQlb0NPRx781b9NowOmUr5s
+            //secret key = RTHB4vGiGHOo + SxxVnQlb0NPRx781b9NowOmUr5s
             //id = AKIAJ4QTGGVMU4SQ4LSQ
-            //email =johnsmith@kreative-i.com
+            //email = johnsmith@kreative - i.com
             //passs = 339229475
-            //AWS Account ID: 	1632-7266-6464
-            //Canonical User ID:  de869bd6b9c3e13acf2c91b2b9ca4c5d50397fc487ed3175358cbf2544a1dbb7 
+            //AWS Account ID: 1632 - 7266 - 6464
+            //Canonical User ID: de869bd6b9c3e13acf2c91b2b9ca4c5d50397fc487ed3175358cbf2544a1dbb7
         }
     }
 }
