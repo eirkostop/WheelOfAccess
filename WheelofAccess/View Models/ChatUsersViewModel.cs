1357b31ab9aa130f80476ca86160e0d206a1acff
Foteini Pipi <pipifot@gmail.com>
2019-08-01 13:57:58 +0300

    Updating SignalR

--- WheelofAccess/View Models/ChatUsersViewModel.cs
+++ WheelofAccess/View Models/ChatUsersViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WheelofAccess.Models
+namespace WheelofAccess.View_Models
 {
     public class ChatUsersViewModel
     {
