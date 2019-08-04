54775dab030b25591de8bd4efbd38ed24bfb7728
Eirini Kostopoulou <lysistrat@hotmail.com>
2019-08-04 16:38:40 +0300

    comment out seed

--- WheelofAccess/Migrations/Configuration.cs
+++ WheelofAccess/Migrations/Configuration.cs
@@ -17,46 +17,46 @@ namespace WheelofAccess.Migrations
 
         protected override void Seed(WheelofAccess.Models.ApplicationDbContext context)
         {
-            var userstore = new UserStore<ApplicationUser>(context);
-            ApplicationUserManager userManager = new ApplicationUserManager(userstore);
-            var rolestore = new RoleStore<IdentityRole>(context);
-            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(rolestore);
+        //    var userstore = new UserStore<ApplicationUser>(context);
+        //    ApplicationUserManager userManager = new ApplicationUserManager(userstore);
+        //    var rolestore = new RoleStore<IdentityRole>(context);
+        //    RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(rolestore);
 
 
-            string email = "admin@gmail.gr";
-            string username = "admin";
-            string password = "Admin1!";
-            //string roleName = "Admin";
-            var roleName = "Admin";
-            var roleName2 = "Regular User";
-            IdentityRole role = roleManager.FindByName(roleName);
-            if (role == null)
-            {
-                role = new IdentityRole(roleName);
-                roleManager.Create(role);
-            }
+        //    string email = "admin@gmail.gr";
+        //    string username = "admin";
+        //    string password = "Admin1!";
+        //    //string roleName = "Admin";
+        //    var roleName = "Admin";
+        //    var roleName2 = "Regular User";
+        //    IdentityRole role = roleManager.FindByName(roleName);
+        //    if (role == null)
+        //    {
+        //        role = new IdentityRole(roleName);
+        //        roleManager.Create(role);
+        //    }
             
 
-            IdentityRole role2 = roleManager.FindByName(roleName2);
-            if (role2 == null)
-            {
-                role2 = new IdentityRole(roleName2);
-                roleManager.Create(role2);
-            }
-            ApplicationUser user = userManager.FindByName(username);
-            if (user == null)
-            {
-                user = new ApplicationUser()
-                {
-                    UserName = email,
-                    Email = email
-                };
-                userManager.Create(user, password);
-            }
-            if (!userManager.IsInRole(user.Id, roleName))
-            {
-                userManager.AddToRole(user.Id, roleName);
-            }
+        //    IdentityRole role2 = roleManager.FindByName(roleName2);
+        //    if (role2 == null)
+        //    {
+        //        role2 = new IdentityRole(roleName2);
+        //        roleManager.Create(role2);
+        //    }
+        //    ApplicationUser user = userManager.FindByName(username);
+        //    if (user == null)
+        //    {
+        //        user = new ApplicationUser()
+        //        {
+        //            UserName = email,
+        //            Email = email
+        //        };
+        //        userManager.Create(user, password);
+        //    }
+        //    if (!userManager.IsInRole(user.Id, roleName))
+        //    {
+        //        userManager.AddToRole(user.Id, roleName);
+        //    }
         }
     }
 }
