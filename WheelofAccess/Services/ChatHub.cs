42c27fb0bc870a8529221a6903ce0fe7fa3b60d2
Foteini Pipi <pipifot@gmail.com>
2019-08-05 14:13:44 +0300

    Chat Image added

--- WheelofAccess/Services/ChatHub.cs
+++ WheelofAccess/Services/ChatHub.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.SignalR;
+using WheelofAccess.Models;
 
 namespace WheelofAccess.Chat_Service
 {
     public class ChatHub : Hub
     {
-        public void sendToUser(string to, string message,byte[] profilepic)
+        public void sendToUser(string to, string message)
         {
-            Clients.User(to).gotMessage(Context.User.Identity.Name, message,profilepic);
+            ApplicationDbContext db = new ApplicationDbContext();
+            ApplicationUser user = db.Users.Find(Context.User.Identity.GetUserId());
+            Clients.User(to).gotMessage(Context.User.Identity.Name, message,user.ProfilePic);
         }
         public void Hello()
         {
